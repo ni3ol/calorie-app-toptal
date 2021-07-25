77866349723807327cb12fd3a5737984ef45f58b
Nicol Vojacek <nvojacek@gmail.com>
2021-07-25 21:18:15 +0200

    Add RailsAdmin gem

--- calorie_app_be/config/routes.rb
+++ calorie_app_be/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/rails_admin', as: 'rails_admin'
   resources :food_entries, only: [:index]
   resources :admin_food_statistics, only: [:index]
 end
