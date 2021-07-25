82de1eb3a1afa6f7b715e56b5924bd309a0af77a
Nicol Vojacek <nvojacek@gmail.com>
2021-07-25 21:14:27 +0200

    Add admin food statistics

---
+++ calorie_app_be/app/controllers/admin_food_statistics_controller.rb
@@ -0,0 +1,40 @@
+class AdminFoodStatisticsController < ApplicationController
+  def index
+    render json: {
+      last_week_entries_count: last_week_entries_count,
+      two_weeks_ago_entries_count: two_weeks_ago_entries_count,
+      current_day_entries_count: current_day_entries_count,
+      last_week_average_calories_per_user_count: last_week_average_calories_per_user_count
+    }
+  end
+
+  private
+
+  def last_week_entries_count
+    FoodEntry.where('created_at >= ?', 1.week.ago).count
+  end
+
+  def two_weeks_ago_entries_count
+    FoodEntry.where(created_at: 2.weeks.ago..1.week.ago).count
+  end
+
+  def current_day_entries_count
+    FoodEntry.where('created_at >= ?', 1.day.ago).count
+  end
+
+  def last_week_average_calories_per_user_count
+    user_to_average_calouries_mapping = {}
+    User.all.each do |user|
+      foods = FoodEntry.where(user_id: user.id).where('created_at >= ?', 1.week.ago)
+      calories = 0
+      foods.each do |food|
+        calories += food.calories
+      end
+
+      seven_day_average = calories / 7
+      user_to_average_calouries_mapping[user.id] = seven_day_average
+    end
+
+    user_to_average_calouries_mapping
+  end
+end
--- calorie_app_be/config/routes.rb
+++ calorie_app_be/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :food_entries, only: [:index]
+  resources :admin_food_statistics, only: [:index]
 end
